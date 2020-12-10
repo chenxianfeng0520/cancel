0ca48808b51a891bdbb3a8ab2d5a22c7bf26f24c
chenxianfeng0520 <chenxianfeng0520@gmail.com>
2020-12-10 15:08:21 +0800

    新增风格

--- i/show.js
+++ i/show.js
@@ -4,15 +4,12 @@ var contextList = [
         id: 1,
         type: 'js',
         title: 'js中this的指向',
+        url:'index1.html?id=1',
         context: [
             [
                 {
                     type: 'text',
-                    text: '1111'
-                },
-                {
-                    type: 'img',
-                    url: './i/1.png'
+                    text: 'this最终指向的是调用它的对象，this最终指向的是调用它的对象，this最终指向的是调用它的对象，this最终指向的是调用它的对象，this最终指向的是调用它的对象，this最终指向的是调用它的对象，'
                 }
             ]
         ]
@@ -20,16 +17,17 @@ var contextList = [
     {
         id: 2,
         type: 'js',
-        title: 'js中this的指向1111',
+        title: '立即执行表达式',
         context: [
             [
                 {
                     type: 'text',
-                    text: '1111'
+                    text: '函数在定义后立即执行',
+                    style:'font-weight: bold;font-size:18px;margin:10px;'
                 },
                 {
-                    type: 'img',
-                    url: './i/1.png'
+                    type: 'html',
+                    html: '1. 不必为函数命名，避免了污染全局变量<br>2. 立即执行函数内部形成了一个单独的作用域，可以封装一些外部无法读取的私有变量<br>3. 封装变量'
                 }
             ]
         ]
